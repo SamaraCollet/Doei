1889eecf32f91216a4f5403b3ba183011ed55307
Raphael Souza <raphaelgs96@gmail.com>
2021-01-13 13:55:56 -0300

    feat: Voluntary register page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import VoluntaryRegister from './pages/voluntary-register'
+import OngRegister from './pages/ong-register'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VoluntaryRegister />
+      <OngRegister />
     </div>
   );
 }
