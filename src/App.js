55253fd1184f37c164c3a4a1db05d5ab437ff9f8
Raphael Souza <raphaelgs96@gmail.com>
2021-01-13 14:35:44 -0300

    Corrigindo divergencias para push na developer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import VoluntaryRegister from './pages/voluntary-register'
-import OngRegister from './pages/ong-register'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <VoluntaryRegister />
-      <OngRegister />
     </div>
   );
 }
