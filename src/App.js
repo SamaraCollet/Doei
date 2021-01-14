cc916b22426fa02c761fd3480f399892235a3dcd
Raphael Souza <raphaelgs96@gmail.com>
2021-01-14 17:47:43 -0300

    feat: pagina de login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Header from "./components/header";
-import VoluntaryRegister from './pages/voluntary-register'
-import OngRegister from './pages/ong-register'
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header />
-      <VoluntaryRegister />
-      <OngRegister />
+      <Header/>
     </div>
   )
 }
