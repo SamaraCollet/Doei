285d700a5285dc33849eccdfa3f5dce6887d0121
Enias Oliveira <eniasoliveira27@gmail.com>
2021-01-15 16:05:37 -0300

    chore: creates campaign reducer

---
+++ src/store/ducks/campaigns.js
@@ -0,0 +1,28 @@
+// Action Types
+
+export const Types = {
+  ALL_CAMPAIGNS: 'campaings/ALL_CAMPAIGNS',
+};
+
+// Reducer
+
+
+const campaignsReducer = (state = [], action) => {
+  switch (action.type) {
+    case Types.ALL_CAMPAIGNS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default campaignsReducer
+
+// Action Creators
+
+export const setAllCampaigns = (campaigns) => {
+  return {
+    type: Types.ALL_CAMPAIGNS,
+    payload: campaigns
+  }
+}
