08ee3d4934c8cf7bded8fc9e74432fef0eabde0b
Samara Sibila Jacowski Collet <samaracollet@gmail.com>
2021-01-21 11:45:37 -0300

    ajustes pagina de perfil do voluntario

--- src/pages/page-not-found/styles.js
+++ src/pages/page-not-found/styles.js
@@ -1,10 +1,30 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
+export const Container = styled.div `
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    padding: 10px 0;
+  }
+  p {
+    font-size: 20px;
+    padding: 5px 0;
+  }
+
+  .link {
+    padding-top: 20px;
+    color: #148cee;
+    font-size: 20px;
+  }
+
+  .big {
+    font-size: 3rem;
+    color: #148cee;
+    font-weight: bold;
+  }
+`;
